0bc55b1d45849575162d83e562ed98e70d504543
Alessandro <alessandro.damico2@stud.unifi.it>
2019-06-28 00:25:22 +0200

    Initial commit

---
+++ RB.py
@@ -0,0 +1,20 @@
+#class BT:
+    #TODO
+    #color
+
+
+class Person(object):
+
+    # Constructor
+    def __init__(self, name):
+        self.name = name
+
+        # To get name
+
+    def getName(self):
+        return self.name
+
+        # To check if this person is employee
+
+    def isEmployee(self):
+        return False
