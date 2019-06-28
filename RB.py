cdad6192ab581623761b8bf22fe73303439014cf
aleda <alessandro.damico2@stud.unifi.it>
2019-06-28 20:36:01 +0200

    Implementati: - Albero - Nodi - Inserimento - InorderWalk

--- RB.py
+++ RB.py
@@ -1,20 +1,57 @@
 #class BT:
-    #TODO
+    #TODO Posso implementarlo come classe?
     #color
+class Vertex:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.p = None
+        self.key = key
+    def getKey(self):
+        return self.key
 
-
-class Person(object):
+class BinaryT:
 
     # Constructor
-    def __init__(self, name):
-        self.name = name
+    def __init__(self):
+        self.root = None
+    def getRoot(self):
+        return self.root
+    def setRoot(self,x):
+        self.root = x
+    def insert(self, z):
 
-        # To get name
+        y = None
+        x = self.root
 
-    def getName(self):
-        return self.name
+        while x is not None:
+            y = x
+            if z.getKey() < x.getKey():
+                x = x.left
+            else:
+                x = x.right
+        z.p = y
+        if y is None:
+            self.setRoot(z)
+        elif z.getKey() < y.getKey():
+            y.left = z
+        else:
+            y.right = z
+    def InorderTreeWalk(self, x):
+        if x is not None:
+            self.InorderTreeWalk(x.left)
+            print(x.key)
+            self.InorderTreeWalk(x.right)
 
-        # To check if this person is employee
 
-    def isEmployee(self):
-        return False
+#class RedBlackT(BinaryT):
+
+T = BinaryT()
+nodo1=Vertex(12)
+nodo2=Vertex(5)
+nodo3=Vertex(14)
+T.insert(nodo1)
+T.insert(nodo2)
+T.insert(nodo3)
+#print(T.getRoot().getKey())
+T.InorderTreeWalk(T.root)
