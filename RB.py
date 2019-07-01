493090b0d9a75dc13c23e27e01fc148e7bd2dc29
Alessandro <alessandro.damico2@stud.unifi.it>
2019-07-01 19:11:48 +0200

    Working BT

--- RB.py
+++ RB.py
@@ -1,7 +1,5 @@
-#class BT:
-    #TODO Posso implementarlo come classe?
-    #color
-class Vertex:
+
+class BstVertex:
     def __init__(self, key):
         self.left = None
         self.right = None
@@ -19,14 +17,14 @@ class BinaryT:
         return self.root
     def setRoot(self,x):
         self.root = x
-    def insert(self, z):
-
+    def insert(self, k):
+        z = BstVertex(k)
         y = None
         x = self.root
 
         while x is not None:
             y = x
-            if z.getKey() < x.getKey():
+            if z.getKey() <= x.getKey():
                 x = x.left
             else:
                 x = x.right
@@ -42,16 +40,43 @@ class BinaryT:
             self.InorderTreeWalk(x.left)
             print(x.key)
             self.InorderTreeWalk(x.right)
+    def findSub(self, x, key):
+        if x is None:
+            return False
+        elif x.getKey() == key:
+            return True
+        elif key < x.getKey():
+            return self.findSub(x.left, key)
+        else:
+            return self.findSub(x.right, key)
+
+    def find(self, key1):
+        return self.findSub(self.root, key1)
 
 
-#class RedBlackT(BinaryT):
+class BrVertex(BstVertex):
+    def __init__(self):
+        self.color = None
+    def setColor(self, col):
+        self.color=col
+    def getColor(self):
+        return self.color
 
 T = BinaryT()
-nodo1=Vertex(12)
-nodo2=Vertex(5)
-nodo3=Vertex(14)
-T.insert(nodo1)
-T.insert(nodo2)
-T.insert(nodo3)
+
+T.insert(4)
+T.insert(7)
+T.insert(2)
+T.insert(6)
+T.insert(5)
+T.insert(1)
+T.insert(7)
+T.insert(3)
+
+print(T.find(10))
 #print(T.getRoot().getKey())
 T.InorderTreeWalk(T.root)
+
+B=BrVertex()
+B.setColor("black")
+print(B.getColor())
